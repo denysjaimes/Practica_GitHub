92b048e59c6afe7d76b8e6fc9b73c533a98ccf52
Joel Mora <joelmora360@gmail.com>
2012-12-08 21:58:36 -0430

    funcion createcliente

--- bancoProject/src/bancoProject/TipoCuenta.java
+++ bancoProject/src/bancoProject/TipoCuenta.java
@@ -4,40 +4,49 @@
  */
 package bancoProject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  *
  * @author Sergio
  */
 public class TipoCuenta {
+
     private long idTipocuenta;
     private String descripcion;
-    private static int cuantos=0;
-    
-    TipoCuenta(String _descripcion)
-    {
+    private static int cuantos = 0;
+
+    TipoCuenta(String _descripcion) {
         descripcion = _descripcion;
         cuantos = cuantos + 1;
         idTipocuenta = cuantos;
     }
-    public void setidTipocuenta(long id)
-    {
-      idTipocuenta = id;
+
+    public void setidTipocuenta(long id) {
+        idTipocuenta = id;
     }
-    public void setDescripcion(String desc)
-    {
-      descripcion =desc;
+
+    public void setDescripcion(String desc) {
+        descripcion = desc;
     }
-    public long getidTipocuenta()
-    {
-      return idTipocuenta;
+
+    public long getidTipocuenta() {
+        return idTipocuenta;
     }
-    public String getDescripcion()
-    {
-       return descripcion;
+
+    public String getDescripcion() {
+        return descripcion;
     }
+
+    static TipoCuenta addTipoCuenta() throws IOException {
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader entrada = new BufferedReader(isr);
+        
+        System.out.println("Ingrese tipo");
+        TipoCuenta t = new TipoCuenta(entrada.readLine());
     
-    public void addTipoCuenta()
-    {
-       
+        return t;
     }
 }
